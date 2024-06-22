d52b3eadc03765a08e087bf5c2aba08a2f83742c
Pratik Sawarn <pandeyshiv006@gmail.com>
2024-06-22 20:31:45 +0530

    feat: detail dashboard data

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import DashMain from "@/components/dashboard/DashMain";
+import DetailedData from "@/components/dashboard/DetailedData";
 import Meetings from "@/components/dashboard/Meetings";
 
 export default function Home() {
   return (
     <>
       <DashMain />
+      <DetailedData />
       <Meetings />
     </>
   );
