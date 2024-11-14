a571ec1eef01cb8287b60295590b8890236950c8
Amit Kumar <alsoamit@gmail.com>
2024-11-14 16:14:34 +0530

    feat: sentiment

--- src/app/(report)/report/sentiment/[id]/page.tsx
+++ src/app/(report)/report/sentiment/[id]/page.tsx
@@ -5,21 +5,18 @@ import React from "react";
 const GET_TEAM_KPIS = `
   query getMeeting($id: ID!) {
     meeting: getMeeting(id: $id) {
-      teamId
-        team {
-          id
           emotions {
             positive
             negative
             neutral
         }
-      }
     }
   }
 `;
 
 async function fetchMeetingData(id: string) {
   const result = await fetchql(GET_TEAM_KPIS, { id });
+  console.log(result);
   if (result.errors) {
     throw new Error("Failed to fetch meeting data");
   }
@@ -38,8 +35,10 @@ export default async function Synchrony({
     <div className="flex items-center justify-center min-h-screen space-y-4 bg-secondary text-secondary-foreground">
       <div className="max-w-sm p-6 scale-150 rounded-xl bg-white/10">
         <div>
-          <h2 className="text-lg font-medium md:pl-10">Global Sentiment</h2>
-          <TeamSentimentDisplay activeTeamData={meeting.team} />
+          <h2 className="text-lg font-medium md:pl-10">Team Sentiment</h2>
+          <TeamSentimentDisplay
+            activeTeamData={{ emotions: meeting.emotions }}
+          />
         </div>
       </div>
     </div>
